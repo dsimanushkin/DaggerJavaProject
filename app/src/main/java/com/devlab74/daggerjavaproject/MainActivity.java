c457440d876970023d901b6ae6e35a95b761ee83
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-13 07:59:19 -0500

    Initial Commit

---
+++ app/src/main/java/com/devlab74/daggerjavaproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.devlab74.daggerjavaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
