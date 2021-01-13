86d3a08deeebe5e4b4c15aea6892e0a4f2de6f34
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-13 11:14:40 -0500

    Added Navigation Drawer

--- app/src/main/java/com/devlab74/daggerjavaproject/ui/main/MainActivity.java
+++ app/src/main/java/com/devlab74/daggerjavaproject/ui/main/MainActivity.java
@@ -7,19 +7,28 @@ import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.devlab74.daggerjavaproject.BaseActivity;
 import com.devlab74.daggerjavaproject.R;
 import com.devlab74.daggerjavaproject.ui.main.posts.PostsFragment;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "MainActivity";
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
 
         testFragment();
     }
@@ -43,5 +52,21 @@ public class MainActivity extends BaseActivity {
         } else {
             return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_profile: {
+                break;
+            }
+            case R.id.nav_posts: {
+                break;
+            }
+        }
+
+        item.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
