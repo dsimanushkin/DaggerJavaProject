074a0bb7afbfe6f10c03f1429da439a58a823acd
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-13 09:29:33 -0500

    LiveData Reactive Streams and Flowables

--- app/src/main/java/com/devlab74/daggerjavaproject/ui/auth/AuthActivity.java
+++ app/src/main/java/com/devlab74/daggerjavaproject/ui/auth/AuthActivity.java
@@ -2,23 +2,30 @@ package com.devlab74.daggerjavaproject.ui.auth;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.bumptech.glide.RequestManager;
 import com.devlab74.daggerjavaproject.R;
+import com.devlab74.daggerjavaproject.models.User;
 import com.devlab74.daggerjavaproject.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class AuthActivity extends DaggerAppCompatActivity {
+public class AuthActivity extends DaggerAppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "AuthActivity";
 
     private AuthViewModel viewModel;
+    private EditText userId;
 
     @Inject
     Drawable logo;
@@ -36,10 +43,40 @@ public class AuthActivity extends DaggerAppCompatActivity {
 
         viewModel = new ViewModelProvider(this, providerFactory).get(AuthViewModel.class);
 
+        userId = findViewById(R.id.user_id_input);
+        findViewById(R.id.login_button).setOnClickListener(this);
+
         setLogo();
+        subscribeObservers();
     }
 
     private void setLogo() {
         requestManager.load(logo).into((ImageView) findViewById(R.id.login_logo));
+    }
+
+    private void subscribeObservers() {
+        viewModel.observeUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(User user) {
+                if (user != null) {
+                    Log.d(TAG, "onChanged: " + user.getEmail());
+                }
+            }
+        });
+    }
+
+    private void attemptLogin() {
+        if (TextUtils.isEmpty(userId.getText().toString())) {
+            return;
+        } else {
+            viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.login_button) {
+            attemptLogin();
+        }
     }
 }
