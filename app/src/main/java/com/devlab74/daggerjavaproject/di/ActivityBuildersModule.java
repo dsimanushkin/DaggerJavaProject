0baa50e9f88463a4d4d84732d6b441d4bf6ee93c
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-13 10:14:37 -0500

    Injecting Profile ViewModel

--- app/src/main/java/com/devlab74/daggerjavaproject/di/ActivityBuildersModule.java
+++ app/src/main/java/com/devlab74/daggerjavaproject/di/ActivityBuildersModule.java
@@ -3,6 +3,7 @@ package com.devlab74.daggerjavaproject.di;
 import com.devlab74.daggerjavaproject.di.auth.AuthModule;
 import com.devlab74.daggerjavaproject.di.auth.AuthViewModelsModule;
 import com.devlab74.daggerjavaproject.di.main.MainFragmentBuildersModule;
+import com.devlab74.daggerjavaproject.di.main.MainViewModelsModule;
 import com.devlab74.daggerjavaproject.ui.auth.AuthActivity;
 import com.devlab74.daggerjavaproject.ui.main.MainActivity;
 
@@ -22,7 +23,8 @@ public abstract class ActivityBuildersModule {
 
     @ContributesAndroidInjector(
             modules = {
-                    MainFragmentBuildersModule.class
+                    MainFragmentBuildersModule.class,
+                    MainViewModelsModule.class
             }
     )
     abstract MainActivity contributeMainActivity();
--- app/src/main/java/com/devlab74/daggerjavaproject/ui/main/profile/ProfileFragment.java
+++ app/src/main/java/com/devlab74/daggerjavaproject/ui/main/profile/ProfileFragment.java
@@ -1,15 +1,19 @@
 package com.devlab74.daggerjavaproject.ui.main.profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.devlab74.daggerjavaproject.R;
+import com.devlab74.daggerjavaproject.viewmodels.ViewModelProviderFactory;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
 
@@ -17,11 +21,22 @@ public class ProfileFragment extends DaggerFragment {
 
     private static final String TAG = "ProfileFragment";
 
+    private ProfileViewModel viewModel;
+
+    @Inject
+    ViewModelProviderFactory providerFactory;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Toast.makeText(getActivity(), "Profile Fragment", Toast.LENGTH_SHORT).show();
 
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "onViewCreated: ProfilFragment was created...");
+
+        viewModel = new ViewModelProvider(this, providerFactory).get(ProfileViewModel.class);
     }
 }
