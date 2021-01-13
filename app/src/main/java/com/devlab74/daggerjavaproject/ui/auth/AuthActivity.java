dbcba6ed2e0e2fd478efdd333e76c15dfee24f2f
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-13 09:43:49 -0500

    Added Authentication Network Resource Class

--- app/src/main/java/com/devlab74/daggerjavaproject/ui/auth/AuthActivity.java
+++ app/src/main/java/com/devlab74/daggerjavaproject/ui/auth/AuthActivity.java
@@ -7,6 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -26,6 +28,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
 
     private AuthViewModel viewModel;
     private EditText userId;
+    private ProgressBar progressBar;
 
     @Inject
     Drawable logo;
@@ -45,6 +48,7 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
 
         userId = findViewById(R.id.user_id_input);
         findViewById(R.id.login_button).setOnClickListener(this);
+        progressBar = findViewById(R.id.progress_bar);
 
         setLogo();
         subscribeObservers();
@@ -55,11 +59,30 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
     }
 
     private void subscribeObservers() {
-        viewModel.observeUser().observe(this, new Observer<User>() {
+        viewModel.observeUser().observe(this, new Observer<AuthResource<User>>() {
             @Override
-            public void onChanged(User user) {
-                if (user != null) {
-                    Log.d(TAG, "onChanged: " + user.getEmail());
+            public void onChanged(AuthResource<User> userAuthResource) {
+                if (userAuthResource != null) {
+                    switch (userAuthResource.status) {
+                        case LOADING: {
+                            showProgressBar(true);
+                            break;
+                        }
+                        case AUTHENTICATED: {
+                            showProgressBar(false);
+                            Log.d(TAG, "onChanged: LOGIN SUCCESS: " + userAuthResource.data.getEmail());
+                            break;
+                        }
+                        case ERROR: {
+                            showProgressBar(false);
+                            Toast.makeText(AuthActivity.this, userAuthResource.message + "\nDid you enter a number between 1 and 10?", Toast.LENGTH_SHORT).show();
+                            break;
+                        }
+                        case NOT_AUTHENTICATED: {
+                            showProgressBar(false);
+                            break;
+                        }
+                    }
                 }
             }
         });
@@ -70,6 +93,14 @@ public class AuthActivity extends DaggerAppCompatActivity implements View.OnClic
             return;
         } else {
             viewModel.authenticateWithId(Integer.parseInt(userId.getText().toString()));
+        }
+    }
+
+    private void showProgressBar(boolean isVisible) {
+        if (isVisible) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.GONE);
         }
     }
 
