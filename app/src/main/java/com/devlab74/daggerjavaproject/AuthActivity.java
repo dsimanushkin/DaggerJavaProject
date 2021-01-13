231fbbb14818eff6cedc406ed89aa5db8be10933
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-13 08:50:19 -0500

    Referencing Multiple Dagger Dependencies within a Single Module and Singleton Scope

--- app/src/main/java/com/devlab74/daggerjavaproject/AuthActivity.java
+++ app/src/main/java/com/devlab74/daggerjavaproject/AuthActivity.java
@@ -1,7 +1,12 @@
 package com.devlab74.daggerjavaproject;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
 
 import javax.inject.Inject;
 
@@ -12,17 +17,20 @@ public class AuthActivity extends DaggerAppCompatActivity {
     private static final String TAG = "AuthActivity";
 
     @Inject
-    String testString;
+    Drawable logo;
 
     @Inject
-    boolean isAppNull;
+    RequestManager requestManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        Log.d(TAG, "onCreate: " + testString);
-        Log.d(TAG, "onCreate: is app null? " + isAppNull);
+        setLogo();
+    }
+
+    private void setLogo() {
+        requestManager.load(logo).into((ImageView) findViewById(R.id.login_logo));
     }
 }
--- app/src/main/java/com/devlab74/daggerjavaproject/di/AppModule.java
+++ app/src/main/java/com/devlab74/daggerjavaproject/di/AppModule.java
@@ -1,6 +1,16 @@
 package com.devlab74.daggerjavaproject.di;
 
 import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.devlab74.daggerjavaproject.R;
+
+import javax.inject.Singleton;
 
 import dagger.Module;
 import dagger.Provides;
@@ -8,14 +18,22 @@ import dagger.Provides;
 @Module
 public class AppModule {
 
+    @Singleton
     @Provides
-    static String someString() {
-        return "This is a test String";
+    static RequestOptions provideRequestOptions() {
+        return RequestOptions.placeholderOf(R.drawable.white_background).error(R.drawable.white_background);
     }
 
+    @Singleton
     @Provides
-    static boolean getApp(Application application) {
-        return application == null;
+    static RequestManager provideGlideInstance(Application application, RequestOptions requestOptions) {
+        return Glide.with(application).setDefaultRequestOptions(requestOptions);
+    }
+
+    @Singleton
+    @Provides
+    static Drawable provideAppDrawable(Application application) {
+        return ContextCompat.getDrawable(application, R.drawable.logo);
     }
 
 }
