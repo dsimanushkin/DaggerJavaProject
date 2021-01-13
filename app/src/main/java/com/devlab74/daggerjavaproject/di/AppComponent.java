330c9ea833610a431e3f5bbe4c27505215b7f78e
dsimanushkin <dsimanushkin74@gmail.com>
2021-01-13 08:26:02 -0500

    Added Dagger Application Component

---
+++ app/src/main/java/com/devlab74/daggerjavaproject/di/AppComponent.java
@@ -0,0 +1,27 @@
+package com.devlab74.daggerjavaproject.di;
+
+import android.app.Application;
+
+import com.devlab74.daggerjavaproject.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component(
+        modules = {
+                AndroidSupportInjectionModule.class
+        }
+)
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+    }
+
+}
